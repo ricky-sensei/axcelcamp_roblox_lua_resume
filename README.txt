48913fd16304898bbfff7ace1345755793cdbdf8
axcelcamp_ricky <axcelcamp.teacher@gmail.com>
2026-02-12 13:07:08 +0900

    add ch2

--- README.txt
+++
@@ -1,1 +0,0 @@
-![Qiita Sync](https://github.com/ricky-sensei/qiita/actions/workflows/qiita_sync_check.yml/badge.svg)
--- lua/test.lua
+++ lua/test.lua
@@ -1,7 +1,32 @@
-part = script.Parent
+local apple = 5
+local point = 0
 
-local function onTouch(hit)
-    -- ここにさわったときやることを書く
-end
+-- 2個たべる
+apple = apple - 2
+point = point + apple * 20
+print(apple)
+print(point)
 
-part.Touched:Connect(onTouch)
+-- 3個もらう
+apple = apple + 3
+point = point + apple * 20
+print(apple)
+print(point)
+
+-- 5個食べる
+apple = apple - 5
+point = point + apple * 20
+print(apple)
+print(point)
+
+-- 2個もらう
+apple = apple + 2
+point = point + apple * 20
+print(apple)
+print(point)
+
+-- 7こもらう
+apple = apple + 7
+point = point + apple * 20
+print(apple)
+print(point)
