fe85cf15d3f06cc53f006ae71cb51843577104b2
axcelcamp_ricky <axcelcamp.teacher@gmail.com>
2026-02-14 08:01:02 +0900

    add function

--- lua/test.lua
+++ lua/test.lua
@@ -1,7 +1,10 @@
-local lemon = true
-if lemon == true then
-    print(lemon)
-else
-    prin
+function giveDamage(hp, damage)
+    hp = hp - damage
 
+    if hp < 0 then
+        hp = 0
+    end
+    return hp
 end
+
+print(giveDamage(100, 20))
