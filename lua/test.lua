36fe20588be5f6fc7a7fbdb1fbf9188a1fae5f17
axcelcamp_ricky <axcelcamp.teacher@gmail.com>
2026-02-13 12:39:13 +0900

    add loop

--- lua/test.lua
+++ lua/test.lua
@@ -1,32 +1,6 @@
-local apple = 5
-local point = 0
+local number = 0
 
--- 2個たべる
-apple = apple - 2
-point = point + apple * 20
-print(apple)
-print(point)
-
--- 3個もらう
-apple = apple + 3
-point = point + apple * 20
-print(apple)
-print(point)
-
--- 5個食べる
-apple = apple - 5
-point = point + apple * 20
-print(apple)
-print(point)
-
--- 2個もらう
-apple = apple + 2
-point = point + apple * 20
-print(apple)
-print(point)
-
--- 7こもらう
-apple = apple + 7
-point = point + apple * 20
-print(apple)
-print(point)
+while number < 10 do
+    print(number .. "回目")
+    number = number + 1
+end
--- note.md
+++ note.md
@@ -6,4 +6,5 @@
 # メモ
 ## qiita apiでつけられるタグは5つまで:超えるとforbiddenが出る
 ## luaコマンドでrepl使えるよ
-
+## 画像はhtmlのimgタグで指定。qiitaの仕様で、style属性は無視されるので、width属性を加える
+   例:`<img src="https://raw.githubusercontent.com/ricky-sensei/axcelcamp_roblox_lua_resume/main/article/img/image.png" alt="変数とデータ①" width=50%>`
